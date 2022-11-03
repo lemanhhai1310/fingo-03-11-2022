2bb7e16570e2882aef0caf52e4d5247d0b8abaac
hailm <hai.lemanh.1310@gmail.com>
2022-11-04 00:42:37 +0700

    feat: footer

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php $data["title"] = "Homepage"; ?>
 <?php $bodyClass = '' ?>
 <?php require "template-parts/layouts/header.php"; ?>
+<!--Sản phẩm nổi bật-->
+<div class="uk-section home__spNoibat uk-background-cover">
+    <div class="uk-container">
+
+    </div>
+</div>
+<!--/Sản phẩm nổi bật-->
+
+<!--Báo chí nói về chúng tôi-->
+<div class="uk-section" uk-height-viewport="expand: true">
+    <div class="uk-container">
+        1
+    </div>
+</div>
+<!--/Báo chí nói về chúng tôi-->
 
 <?php require "template-parts/layouts/footer.php"; ?>
--- template-parts/layouts/footer.php
+++ template-parts/layouts/footer.php
@@ -1,4 +1,150 @@
+<footer class="footer uk-position-relative">
+    <div class="footer__bgCanvas uk-cover-container uk-position-top-center">
+        <canvas width="2194" height="386"></canvas>
+    </div>
+    <div class="footer__content uk-position-relative">
+        <div class="uk-container">
+            <div class="footer__item-48-20 footer__content__top">
+                <div class="footer__content__boxForm uk-flex uk-flex-middle uk-cover-container uk-background-image@m uk-background-cover" data-src="images/Frame498.png" uk-img>
+                    <div class="uk-width-1-1 uk-card uk-card-body uk-light">
+                        <div class="uk-flex-middle uk-grid-small uk-grid-60-m uk-child-width-auto@s uk-flex-center" uk-grid>
+                            <div>
+                                <h3 class="footer__content__boxForm__title uk-h3 uk-margin-remove">Để lại email nhận ưu đãi từ Fingo</h3>
+                            </div>
+                            <div>
+                                <form class="uk-position-relative">
+                                    <input class="footer__content__boxForm__input uk-border-pill uk-input uk-form-large" type="text" placeholder="Email Address" aria-label="Large">
+                                    <button type="button" class="footer__content__boxForm__btnSubmit uk-button uk-button-large uk-border-pill uk-button-secondary" uk-icon="reply"></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__item-48-20">
+                <div class="uk-child-width-1-4@m uk-child-width-1-2 uk-grid-20 uk-grid-30-m" uk-grid>
+                    <div>
+                        <div class="footer__item-19-15">
+                            <h4 class="uk-h4 uk-margin-remove footer__content__title">Vay tiền nhanh</h4>
+                        </div>
+                        <div class="footer__item-19-15">
+                            <ul class="footer__content__nav uk-nav uk-nav-default">
+                                <li><a href="">Tamo</a></li>
+                                <li><a href="">Senmo</a></li>
+                                <li><a href="">Vamo</a></li>
+                                <li><a href="">Robocash</a></li>
+                                <li><a href="">ATM online</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="footer__item-19-15">
+                            <h4 class="uk-h4 uk-margin-remove footer__content__title">Vay tiền trả góp</h4>
+                        </div>
+                        <div class="footer__item-19-15">
+                            <ul class="footer__content__nav uk-nav uk-nav-default">
+                                <li><a href="">Mirae Asset Finance</a></li>
+                                <li><a href="">MCredit</a></li>
+                                <li><a href="">Shinhan finance</a></li>
+                                <li><a href="">Lotte Finance</a></li>
+                                <li><a href="">Easy Credit</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="footer__item-19-15">
+                            <h4 class="uk-h4 uk-margin-remove footer__content__title">Vay tín chấp</h4>
+                        </div>
+                        <div class="footer__item-19-15">
+                            <ul class="footer__content__nav uk-nav uk-nav-default">
+                                <li><a href="">VpBank</a></li>
+                                <li><a href="">Vietcombank</a></li>
+                                <li><a href="">Shinhan Bank</a></li>
+                                <li><a href="">ABBank</a></li>
+                                <li><a href="">Tinvay</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="footer__item-19-15">
+                            <h4 class="uk-h4 uk-margin-remove footer__content__title">Cầm đồ online</h4>
+                        </div>
+                        <div class="footer__item-19-15">
+                            <ul class="footer__content__nav uk-nav uk-nav-default">
+                                <li><a href="">F88</a></li>
+                                <li><a href="">TienNgay</a></li>
+                                <li><a href="">Tima</a></li>
+                                <li><a href="">Vietmoney</a></li>
+                                <li><a href="">Dongshopsun</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__item-48-20">
+                <div class="footer__item-24-20">
+                    <div class="uk-child-width-1-2@m uk-grid-20 uk-grid-30-m" uk-grid>
+                        <div>
+                            <div class="footer__item-19-15">
+                                <h4 class="uk-h4 uk-margin-remove footer__content__title">Thông tin</h4>
+                            </div>
+                            <div class="footer__item-19-15">
+                                <div class="footer__content__desc">Fingo Việt Nam được thành lập vào năm 2015. Dựa trên nền tảng công nghệ, Fingo cam kết mang đến những giải pháp tài chính tối ưu nhất đến với khách hàng.</div>
+                            </div>
+                            <div class="footer__item-30-15">
+                                <div class="uk-child-width-auto@s uk-child-width-1-2" uk-grid>
+                                    <div>
+                                        <a href=""><img src="images/Frame-logo.png" alt=""></a>
+                                    </div>
+                                    <div>
+                                        <a href=""><img src="images/DMCA_logo-std-btn225w1.png" alt=""></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="footer__item-19-15">
+                                <h4 class="uk-h4 uk-margin-remove footer__content__title">Trụ sở chính</h4>
+                            </div>
+                            <div class="footer__item-19-15">
+                                <ul class="footer__content__listAddress uk-list uk-margin-remove">
+                                    <li class="maps">Trụ sở chính: 15 Thân Nhân Trung, P13, Tân Bình, TPHCM</li>
+                                    <li class="phone">Hotline: 08 999 38383</li>
+                                    <li class="email">Email: info@fingo.vn</li>
+                                </ul>
+                            </div>
+                            <div class="footer__item-48-20">
+                                <a href="" class="uk-icon-button uk-margin-small-right" uk-icon="twitter"></a>
+                                <a href="" class="uk-icon-button  uk-margin-small-right" uk-icon="facebook"></a>
+                                <a href="" class="uk-icon-button" uk-icon="youtube"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer__item-24-20">
+                    <ul class="footer__content__subnav uk-subnav uk-subnav-divider uk-margin-remove-bottom uk-flex-center">
+                        <li><a href="#">Chính sách bảo mật</a></li>
+                        <li><a href="#">Quy chế hoạt động</a></li>
+                        <li><a href="#">Chính sách đổi tra</a></li>
+                        <li><a href="#">Cơ hội việc làm</a></li>
+                        <li><a href="#">Liên hệ</a></li>
+                    </ul>
+                </div>
+                <div class="footer__item-24-20">
+                    <div class="uk-position-relative footer__content__line">
+                        <hr class="uk-margin-remove">
+                    </div>
+                </div>
+                <div class="footer__item-24-20 uk-text-center">
+                    <div class="footer__content__copyright">© 2021 - Bản quyền thuộc về tập đoàn Fingo Việt Nam  ("Fingo"). Fingo sử dụng nền tảng công nghệ trí tuệ nhân tạo để kết nối người đi vay với đối tác cho vay, Fingo không hoạt động như người đi vay hay đối tác cho vay. Tuyên bố miễn trừ trách nhiệm : Tất cả các ưu đãi và sản phẩm trên nền tảng fingo.vn đại diện cho dữ liệu sản phẩm hiện tại và không ràng buột người nộp đơn trước khi hợp đồn được ký kết và áp dụng. Thông tin sản phẩm trên trang web fingo.vn có thể thay đổi bất kỳ lúc nào mà không cần thông báo trước để luôn cập nhật những thông tin và mức giá mới nhất. HIện tại tất cả các sản phẩm chỉ có sẵn tại Việt Nam</div>
+                </div>
+                <div class="footer__item-24-20">
 
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 </div>
 <!--/app-->
 </body>
